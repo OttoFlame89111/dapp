7fac5ab9492f0acf3d7db981433570a25761f76f
蔡哲軒 <>
2022-12-19 20:53:11 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- "connect" click event is registered -->
+    <button v-if="!connected" @click="connect">Connect wallet</button>
+    <button v-if="connected">Call contract</button>
+    {{ contractResult }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      connected: false,
+      contractResult: '',
+    }
+  },
+
+  methods: {
+
+    connect: function () {
+        // this connects to the wallet
+      
+      if (window.ethereum) { // first we check if metamask is installed
+        window.ethereum.request({ method: 'eth_requestAccounts' })
+          .then(() => {
+            this.connected = true; // If users successfully connected their wallet
+          });
+      }
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
